a69605eb3fc74e09289df3c1e63c0a7e06ea96e6
bmellborges <brunomelloborges@outlook.com>
2019-02-26 20:13:49 -0300

    Base do projeto

---
+++ footer.php
@@ -0,0 +1,43 @@
+
+<html>
+    <head>
+        <title>footer</title>
+    </head>
+    <body>
+        <footer>
+            
+                <div class="card-footer text-center border-primary">
+                    <p class="badge-dark" >Mais informações</p>
+                    <div class="row">
+                        <div class="col-lg-auto">
+                            <div col-xl-6 class="card-footer text-center" >
+                              <div class="card-body">
+                                <h5 class="card-title">Mais informações</h5>
+                                <p class="card-text text-center" >With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-auto">
+                            <div class="card-footer text-center" >
+                              <div class="card-body">
+                                <h5 class="card-title">Mais informações</h5>
+                                <p class="card-text text-center" >With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-auto">
+                            <div class="card-footer text-center" >
+                              <div class="card-body">
+                                <h5 class="card-title">Mais informações</h5>
+                                <p class="card-text text-center" >With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+    include ('navbar.php');
+?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div class="container-fluid align-self-lg-center" id="corpo">
+            <div class="row justify-content-lg-around">
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+            </div>
+            <div class="row justify-content-lg-around">
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+            </div>
+            <div class="row justify-content-lg-around">
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+            </div>
+            <div class="row justify-content-lg-around">
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+                <div class="col-2">
+                    <img src="img/parfum.jpg" alt="imagem-perfume">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<?php
+include 'footer.php';
+?>
