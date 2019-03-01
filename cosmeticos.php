bb31f71210ad2ee2bcddf3efe4572e167729998d
bmellborges <brunomelloborges@outlook.com>
2019-03-01 10:07:27 -0300

    ADD Modelos DER E MER

---
+++ cosmeticos.php
@@ -0,0 +1,124 @@
+<?php
+    include ('navbar.php');
+?>
+<html>
+    
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body background="img/marmore.jpg">
+        <div class="container-fluid align-self-lg-center" id="corpo">
+            <div class="row justify-content-lg-around">
+                
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/batom2.png" class="card-img-top" alt="imagem-perfume">
+  <div class="card-body">
+    <h5 class="card-title">Rouge coco</h5>
+    <p class="card-text">ROUGE COCO, o icônico batom CHANEL, reinventado. A nova fórmula proporciona maior sensorialidade e hidratação durante todo o dia. Uma nova paleta inspirada nos amigos de Mademoiselle Chanel, nos que a chamavam simplesmente de Coco. 24 cores compõem o conjunto ROUGE COCO.</p>
+  </div>
+</div>
+                
+            <div class="card" style="width: 18rem;">
+  <img src="img/batom3.png" class="card-img-top" alt="imagem-perfume">
+  <div class="card-body">
+    <h5 class="card-title">Vult 01</h5>
+    <p class="card-text">Batom com acabamento matte. Vult Make Up Matte possui uma variedade de cores com alta cobertura e duração para lábios realçados.</p>
+  </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/sun.jpg" class="card-img-top" alt="imagem-perfume">
+  <div class="card-body">
+    <h5 class="card-title">Kadus</h5>
+    <p class="card-text">Ajuda a colorir seu cabelo, protege-lo e deixa-lo mais luminoso.</p>
+  </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/erva.jpg" class="card-img-top" alt="imagem-perfume">
+  <div class="card-body">
+    <h5 class="card-title">Ana maria 29 ervas</h5>
+    <p class="card-text">O gel massageador Ana Maria é um produto cosmético desenvolvido com a combinação de cânfora, mentol, salicilato de metila e 29 ervas naturais. Promove sensação de conforto e bem estar para as costas, pernas e pés através de boa massagem.</p>
+  </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/kem.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Kyed well</h5>
+    <p class="card-text"> Shampoo para cabelos secos, ressecados ou quimicamente tratados. Wella Professionals Invigo Nutri-Enrich Shampoo tem o poder de hidratar e nutrir os fios desde o momento da lavagem e, assim, vai muito além de limpar.</p>
+    
+  </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/nano.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Kit Gol Pantene</h5>
+    <p class="card-text">Fortalece o cabelo desde o interior e limpa efetivamente, removendo sujeira e resíduos já no primeiro uso
+o Aplique diariamente ou a cada lavagem.</p>
+    </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/mira.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Blond Therapy</h5>
+    <p class="card-text">Serviço profissional de alisamento com remoção de tons alaranjados, para cabelos loiros, com luzes ou mechas. Restaura o esplendor do loiro ao mesmo tempo em que repara as pontas duplas, preenche a porosidade dos fios e alisa, sem os danos das químicas agressivas.</p>
+    </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/fixa.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Ruby Rose</h5>
+    <p class="card-text">Serviço profissional de alisamento com remoção de tons alaranjados, para cabelos loiros, com luzes ou mechas. Restaura o esplendor do loiro ao mesmo tempo em que repara as pontas duplas, preenche a porosidade dos fios e alisa, sem os danos das químicas agressivas.</p>
+    </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/vul.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">BB Cream</h5>
+    <p class="card-text">Vult BB Cream e CC Cream para todos os tipos de pele. Vult suaviza os poros e protege sua pele do sol enquanto proporciona hidratação intensa. Proteja sua pele e fique linda ao mesmo tempo com Vult!.</p>
+  </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/vult.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Vult Make Up</h5>
+    <p class="card-text">Batom de acabamento cremoso. Bem pigmentado, dá cor enquanto realça e define a boca. Tanto para o cotidiano como uma noite de balada, Vult Make Up Batom dura por longo tempo. Ele promove alta cobertura, fácil deslize e ótima fixação.</p>
+  </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/sobra.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Tracta</h5>
+    <p class="card-text">Oferece efeito natural, preenche, define e é de longa duração. Tudo isso sem deixar resíduos. O maravilhoso aplicador associado à textura em gel alcança todos os fios da sobrancelha de maneira uniforme e de uma só vez com efeito imediato e natural.</p>
+    </div>
+</div>
+                
+                <div class="card" style="width: 18rem;">
+  <img src="img/ruby.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Primer Olhos</h5>
+    <p class="card-text">Primer para os olhos RUBY ROSE é um creme com textura leve à base de água e sem óleo. Possui uma embalagem versátil com um aplicador fácil de aplicar. Após aplicação o produto seca rápido deixando uma sensação confortável. Possui ótima fixação e duração, realça a cor permitindo que sua sombra (compacta ou em pó ou cremosa, pigmento ou glitter) fique intacta e realçada o dia todo.</p>
+    </div>
+</div>
+                
+            
+            </div>
+            </div>
+            
+        
+        
+    </body>
+</html>
+<?php
+include 'footer.php';
+?>
